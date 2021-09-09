dd4eb3ac110de46d1e98b11082b1942cdceac6bf
abran <alexandru.bran@nokia.com>
2021-09-09 19:35:29 +0300

    draft form for filtering and table

--- client/src/components/IncoherenceReporting.tsx
+++ client/src/components/IncoherenceReporting.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import "./IncoherenceReporting.scss"
-
+import moment from "moment";
 import { useMutation, useQuery, gql } from "@apollo/client";
+
+import { Line } from 'react-chartjs-2';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 
 import ExcelReader from "./ExcelReader";
 import Button from '@material-ui/core/Button';
@@ -16,6 +19,9 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Autocomplete from "@material-ui/lab/Autocomplete";
 import TextField from '@material-ui/core/TextField';
+
+import { useForm } from 'react-hook-form'
+import { Modal, Form } from 'react-bootstrap'
 
 const SAVE_DATA = gql`
 mutation ($data: [Incoherence], $week: String!) {
@@ -49,11 +55,15 @@ const useStyles = makeStyles({
 
 const IncoherenceReporting = () => {
   const newDate = new Date()
+  const [allCheck, setAllCheck] = useState(false);
   const classes = useStyles();
   const [fileData, setFileData] = useState();
-  const [showUploadModal, setShowUploadModal] = useState(true)
+  const [showUploadModal, setShowUploadModal] = useState(false)
   const [selectedWeek, setSelectedWeek] = useState()
+  const { register, handleSubmit, errors } = useForm();
 
+  
+  const date = moment().format("YYYY-MM-DD")
 
   const sendData = (data) => {
     var that = this;
@@ -70,6 +80,18 @@ const IncoherenceReporting = () => {
     )
   }
 
+  const onSubmit = (data) => {
+
+    apiService.graphql({
+      query: GET_REPORTING,
+      variables: { startDate: data.startDate, endDate: data.endDate },
+    })
+      .then((response) => {
+        console.log(response)
+      })
+    }
+
+
 
 
   const [saveDataMutation] = useMutation(SAVE_DATA, {
@@ -84,7 +106,86 @@ const IncoherenceReporting = () => {
     onError: (error) => { console.error("Error creating a post", error); alert("Error creating a post request " + error.message) },
   });
 
-
+  const lineChartData = {
+    data: {
+      labels: ['W1', 'W2'],
+      datasets: [
+        {
+          type: 'line',
+          label: 'Taux de fiabilite',
+          data: [1,2],
+          yAxisID: 'A',
+          fill: false,
+          // backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(54, 162, 235)',
+          borderWidth: 2,
+          datalabels: {
+            color: 'white',
+            font: 'bold',
+            backgroundColor: 'black',
+            borderColor: 'rgb(54, 162, 235)',
+            borderWidth: 2
+          }
+  
+        },
+        // {
+        //   label: "Cellules presentes en BDR ou RR uniquement",
+        //   yAxisID: 'B',
+        //   backgroundColor: "red",
+        //   data: firstCat,
+        //   stack: 2
+        // },
+        // {
+        //   label: "Cellules demontees, mais presentes en BDR ou RR",
+        //   yAxisID: 'B',
+        //   backgroundColor: "orange",
+        //   data: secondCat,
+        //   stack: 2
+        // },
+        // {
+        //   label: "Cellules normales",
+        //   yAxisID: 'B',
+        //   backgroundColor: "green",
+        //   data: thirdCat,
+        //   stack: 2
+        // }
+  
+      ],
+    },
+    options: {
+      plugins: {
+        datalabels: {
+          color: 'white',
+          font: {
+            // weight: 'bold',
+            size: 14
+          }
+        }
+      },
+      scales: {
+        A: {
+          type: 'linear',
+          position: 'left',
+          title: {
+            display: true,
+            text: '%'
+          }
+        },
+        B: {
+          type: 'linear',
+          position: 'right',
+          title: {
+            display: true,
+            text: 'Nombre de cellules'
+          },
+          ticks: {
+            // max: 1,
+            // min: 1000
+          }
+        }
+      },
+    }
+  }
 
 
   const getWeek = (date) => {
@@ -137,14 +238,39 @@ const IncoherenceReporting = () => {
             }}
           />
           {selectedWeek ? <div>Selected week is : <b>{selectedWeek}</b></div> : null}
-          <div>
-            <Button variant="contained" color="primary" disabled={selectedWeek? false : true} onClick={() => saveData()}>Save</Button>
-          </div>
-          <div>
-            <Button variant="contained" color="secondary"  onClick={() =>  {setFileData(null);setSelectedWeek(null);}}>Close</Button>
+          <div className="button-container" >
+            <Button variant="contained" className="button" color="primary" disabled={selectedWeek ? false : true} onClick={() => saveData()}>Save</Button>
+
+            <Button variant="contained" color="secondary" onClick={() => { setFileData(null); setSelectedWeek(null); }}>Close</Button>
           </div>
         </div>
         : null}
+    </div>
+    <div className="chart-container">
+      Incoherence Reporting Weekly Values (by Technology)
+      <Form className="reportingForm" onSubmit={handleSubmit(onSubmit)}>
+        <Form.Group controlId="new">
+          {/* <Form.Label aria-invalid={errors.date ? "true" : "false"}>Start Week</Form.Label> */}
+          {/* <Form.Control type="text" name="startWeek"  onChange={(e) => { setStartDate(e.target.value) }}  ref={register({ required: true })} />
+          {/* {errors.date && errors.date.type === "required" && (
+            <span role="alert">This is required</span>
+          )} */}
+          {/* <Form.Label aria-invalid={errors.date ? "true" : "false"}>End Week</Form.Label> */}
+          {/* <Form.Control type="text" name="endWeek"  onChange={(e) => { setEndDate(e.target.value) }}  ref={register({ required: true })} /> */} */}
+          {/* {errors.date && errors.date.type === "required" && (
+            <span role="alert">This is required</span>
+          )} */}
+        </Form.Group>
+        <button type="submit" disabled={allCheck} className="centerBtn btn btn-success">Refresh</button>
+      </Form>
+      <Line
+        data={lineChartData.data}
+        options={lineChartData.options}
+        plugins={[ChartDataLabels]}
+        height={200}
+        width={250}
+      //  options={} 
+      />
     </div>
     <ExcelReader
       setShowModal={() => setShowUploadModal(!showUploadModal)}
