909099c0c423b7197f3516761974b995b180bd95
abran <alexandru.bran@nokia.com>
2021-09-09 07:47:08 +0300

    url redirect test

--- client2/src/redux/reducers/types.js
+++
@@ -1,4 +0,0 @@
-export const UPDATE_PROFILE_TYPE = "UPDATE_PROFILE_TYPE";
-export const AUTH_SIGN_IN = "AUTH_SIGN_IN";
-export const AUTH_SIGN_OUT = "AUTH_SIGN_OUT";
-export const AUTH_ERROR = "AUTH_ERROR";
