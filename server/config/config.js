9df0b476b643be92f2f12de1fa60d97daf9d0d4c
abran <alexandru.bran@nokia.com>
2021-11-17 16:32:02 +0200

    client secret update

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,7 @@ var config = {
     // Azure Application details
     base: process.env.AAD_AUTH_URL || 'https://login.microsoftonline.com/',
     clientID: process.env.AAD_AUTH_CLIENTID || 'ae919e4c-3cba-44d7-80d6-f94ea0898d7d',
-    clientSecret: process.env.AAD_AUTH_CLIENTSECRET || '~GGPP7dpehaav.Y.jLci8enaw~M5ibT_23',
+    clientSecret: process.env.AAD_AUTH_CLIENTSECRET || 'khk7Q~uWLrWBtqArp2L6_EZCaUrNSp7x2hjv2',
     callbackUri: hostUrl + '/auth/cbAdfs',
     resource: process.env.MS_GRAPH_URL || 'https://graph.microsoft.com/',
     tenant: process.env.AAD_AUTH_TENANT || '5d471751-9675-428d-917b-70f44f9630b0'
