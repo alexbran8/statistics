44060237faa4c449ff09c843fb065ed6e755d978
abran <alexandru.bran@nokia.com>
2022-04-15 11:10:56 +0300

    styling of traitment

--- client/src/components/Traitment/Traitment.tsx
+++ client/src/components/Traitment/Traitment.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import "./Traitment.scss"
 import ReactFileReader from 'react-file-reader';
 import Button from '@material-ui/core/Button'
-import JSZip from 'jszip';
+import JSZip, { file } from 'jszip';
 
 
 export const Traitment = () => {
@@ -18,17 +18,20 @@ export const Traitment = () => {
                 var newResults = []
                 var newProgress = progress
                 // setProcessStatus(processStatus + Object.keys(zip.files).length)
-                Object.keys(zip.files).forEach((file,index) => {
+                Object.keys(zip.files).forEach((file, index) => {
                     zip.files[file].async('string').then(function (fileData) {
-                        console.log(index)
-                        var updatedResults = [];
-                        let updatedFiles = []
-                        files.push({ fileName: file, content: fileData, status: 'read-from-client' })
-                        updatedFiles = [...files]
-                        newProgress++
-                        setProgress(newProgress)
-                        setFiles(updatedFiles)
-                    })                
+                        // check if file exists in object if not add them
+                        if (files.find(item => item.fileName === file) === undefined) {
+                            //if not add file to archive
+                            var updatedResults = [];
+                            let updatedFiles = []
+                            files.push({ fileName: file, content: fileData, status: 'read-from-client' })
+                            updatedFiles = [...files]
+                            newProgress++
+                            setProgress(newProgress)
+                            setFiles(updatedFiles)
+                        }
+                    })
                 })
             })
             .catch(error => {
@@ -48,11 +51,10 @@ export const Traitment = () => {
                 <ReactFileReader multipleFiles={false} fileTypes={[".zip"]} handleFiles={handleZip}>
                     <Button variant="contained" color="primary" className='btn'>Click here to upload ZIP file</Button>
                 </ReactFileReader>
-
                 <>
-                    {files ? <>
-                        <div className="process-status-container"> 
-                        <div className="text"> {processStatus !== 0 ?(progress / processStatus * 100).toFixed(1) +'%' : 'waiting for files'}</div></div>
+                    <div className="process-status-container">
+                        <div className="text"> {processStatus !== 0 ? (progress / processStatus * 100).toFixed(1) + '%' : 'waiting for files'}</div></div>
+                    {files && files.length > 0 ? <>
                         <table className='ransharing-table'>
                             <thead>
                                 <tr>
@@ -63,14 +65,12 @@ export const Traitment = () => {
                                 </tr>
                             </thead>
                             {files.map((file, index) => {
-                                return <tr key={index}>    
-                                    <td>{index+1}</td>
+                                return <tr key={index}>
+                                    <td>{index + 1}</td>
                                     <td>{file.fileName}</td>
                                     <td>{file.content.substring(0, 50)}</td>
                                     <td>{file.status}</td>
                                 </tr>
-                                
-
                             })}
 
                         </table>
