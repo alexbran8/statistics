488d527ce778c356a083d5f6f37b519a639b183a
abran <alexandru.bran@nokia.com>
2022-04-15 07:38:14 +0300

    files table display

--- client/src/components/Traitment/Traitment.tsx
+++ client/src/components/Traitment/Traitment.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import "./Traitment.scss"
 import ReactFileReader from 'react-file-reader';
 import Button from '@material-ui/core/Button'
@@ -7,17 +7,23 @@ import JSZip from 'jszip';
 
 export const Traitment = () => {
     const [status, setStatus] = useState < String > ('');
-
+    const [files, setFiles] = useState([]);
+    const [processStatus, setProcessStatus] = useState(0);
+    const [progress, setProgress] = useState(0);
 
     const handleZip = (zipFile) => {
         const zip = new JSZip();
         zip.loadAsync(zipFile[0])
             .then(function (zip) {
                 var newResults = []
+                setProcessStatus(Object.keys(zip.files).length)
                 Object.keys(zip.files).forEach(file => {
                     zip.files[file].async('string').then(function (fileData) {
                         var updatedResults = [];
-                        console.log(fileData)
+                        let updatedFiles = []
+                        files.push({ fileName: file, content: fileData, status: 'read-from-client' })
+                        updatedFiles = [...files]
+                        setFiles(updatedFiles)
                     })
                 })
             })
@@ -26,6 +32,15 @@ export const Traitment = () => {
                 console.log(error.message)
                 setStatus(error.message)
             })
+    }
+
+    const statusLoading = (index) => {
+        console.log(progress)
+        let updatedIndex = index += 1
+        // setProgress(updatedIndex)
+        console.log(updatedIndex)
+        // let progressUpdate = index;
+        // setProgress(updatedIndex);
     }
 
     return (
@@ -39,6 +54,34 @@ export const Traitment = () => {
                     <Button variant="contained" color="primary" className='btn'>Click here to upload ZIP file</Button>
                 </ReactFileReader>
 
+                <>
+                    {files ? <>
+                        <h5>/ {processStatus}</h5>
+                        <table className='ransharing-table'>
+                            <thead>
+                                <tr>
+                                    <td>ID</td>
+                                    <td>FILE</td>
+                                    <td>CONTENT</td>
+                                    <td>STATUS</td>
+                                </tr>
+                            </thead>
+                            {files.map((file, index) => {
+                                statusLoading()
+                                return <tr key={index}>    
+                                    <td>{index}</td>
+                                    <td>{file.fileName}</td>
+                                    <td>{file.content.substring(0, 50)}</td>
+                                    <td>{file.status}</td>
+                                </tr>
+                                
+
+                            })}
+
+                        </table>
+                    </>
+                        : null}
+                </>
             </div>
         </>
     )
