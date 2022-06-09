41df135339fb207305c3cf85f64f2e308a63139c
abran <alexandru.bran@nokia.com>
2022-06-09 09:52:18 +0300

    ransharing 4G

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -10,6 +10,7 @@ import IncoherenceReporting from "./components/IncoherenceReporting";
 import Reporting from "./components/Reporting/Reporting";
 
 import { Ransharing } from "./components/Ransharing/Ransharing";
+import { Ransharing4G } from "./components/Ransharing_4G/Ransharing_4G";
 
 import { Header } from "./components/Header/Header";
 import { HashRouter, Route, Routes } from "react-router-dom";
@@ -28,10 +29,11 @@ export const AppRouter = (props) => {
       <Routes>
         <Route exact path={"/"} element={<Homepage />} />
         <Route exact path={"/error"} element={<LoginError />} />
-        <Route exact path={"/incoherence_reporting"} element={isAuthentificated ? <IncoherenceReporting /> : <Homepage /> } />
-        <Route exact path={"/ransharing"} element={isAuthentificated ? <Ransharing /> : <Homepage /> } />
-        <Route exact path={"/reporting"} element={isAuthentificated ? <Reporting /> : <Homepage /> } />
-        <Route exact path={"/traitment"} element={isAuthentificated ? <Traitment /> : <Homepage /> } />
+        <Route exact path={"/incoherence_reporting"} element={isAuthentificated ? <IncoherenceReporting /> : <Homepage />} />
+        <Route exact path={"/ransharing"} element={isAuthentificated ? <Ransharing /> : <Homepage />} />
+        <Route exact path={"/ransharing_4g"} element={isAuthentificated ? <Ransharing4G /> : <Homepage />} />
+        <Route exact path={"/reporting"} element={isAuthentificated ? <Reporting /> : <Homepage />} />
+        <Route exact path={"/traitment"} element={isAuthentificated ? <Traitment /> : <Homepage />} />
       </Routes>
     </HashRouter>
   );
